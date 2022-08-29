4c9edacdac7d79e49cb105ab367c8603cd9c02b9
Heba989 <106240892+Heba989@users.noreply.github.com>
2022-08-30 01:53:09 +0300

    Update send.py
    Second API request was added

--- send.py
+++ send.py
@@ -7,3 +7,6 @@ print(x.text)
 
 # >>> files = {'image': open(r'C:\Users\user\Desktop\ICT\Python\11751.png','rb')}
 # >>> x = requests.post('http://127.0.0.1:5000/predict', files=files)
+
+rep= requests.get('http://127.0.0.1:5000/retrieve',{'time':'flag="1"'})
+print(rep.text)
