f9d313ee17f71d517a0a0340f1954d2bc911c1e2
Heba989 <106240892+Heba989@users.noreply.github.com>
2022-09-07 13:40:25 +0300

    comments out
    comment out the code

--- send.py
+++ send.py
@@ -1,5 +1,6 @@
 import requests
 
+#requests to pmake prediction
 url = 'http://127.0.0.1:5000/predict'
 files = {'image': open(r'C:Users/user/Desktop/ICT/Python/11751.png','rb')}
 x = requests.post(url, files=files)
@@ -7,6 +8,6 @@ print(x.text)
 
 # >>> files = {'image': open(r'C:\Users\user\Desktop\ICT\Python\11751.png','rb')}
 # >>> x = requests.post('http://127.0.0.1:5000/predict', files=files)
-
+#request to retrieve data as csv file 
 rep= requests.get('http://127.0.0.1:5000/retrieve',{'time':'flag="1"'})
 print(rep.text)
